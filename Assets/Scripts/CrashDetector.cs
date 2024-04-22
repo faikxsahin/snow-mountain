484e1d53050c498401e2c8fd5ea3cfa7fdbadfd9
faikxsahin <faikxsahin@outlook.com>
2024-04-22 11:34:58 +0300

    Added crash sequence fundamentals

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    void OnTriggerEnter2D(Collider2D other) 
     {
         
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Ouch, I hit my head!");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
